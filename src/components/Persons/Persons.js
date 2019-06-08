78cd2a3ca2cf40a742f2d84aceeca61ed113c0de
Rajkumar <rajkanbu@let.com>
2019-06-08 14:36:40 +0530

    #Commit Before the Git Tips

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Person from './Person/Person';
 
 const persons = props =>
-	props.persons.map((person, index) => {
+	props.persons.map((person, indexx) => {
 		return (
 			<Person
 				name={person.name}
 				age={person.age}
 				key={person.id}
-				clickk={() => props.clicked(index)}
+				clickk={() => props.clicked(indexx)}
 				// clickk={() => this.deleteHandler(index)}
 				changee={event => props.changeValue(event, person.id)}
 			/>
